6ea5592c5ba0743ffe9962504a9afdc67d543612
GeometricalTaco <92734466+GeometricalTaco@users.noreply.github.com>
2023-02-03 15:26:33 +0000

    Preparation for databases
    - Added template for an input that utilises AsyncStorage

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import "react-native-gesture-handler"
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
-import { Button, Text, TextInput, View, Image, Pressable, ScrollView, FlatList, SectionList, StyleSheet, SafeAreaView } from "react-native";
+import React, {useState, useEffect} from "react";
+import { Button, Text, TextInput, View, Image, Pressable, ScrollView, FlatList, SectionList, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 import SelectDropdown from "react-native-select-dropdown";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -9,9 +9,11 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+let STORAGE_KEY = "@user_input";
 
 
 
@@ -270,13 +272,73 @@ function ViewLoadoutsScreen({ navigation }) {
   );
 }
 
+
+const saveData = async () => {
+  try {
+    await AsyncStorage.setItem(STORAGE_KEY, age)
+    alert("Data successfully saved")
+  } catch (e) {
+    alert("Failed to save the data to the storage")
+  }
+}
+
+const readData = async () => {
+  try {
+    const value = await AsyncStorage.getItem(STORAGE_KEY);
+
+    if (value !== null) {
+      setInput(value);
+    }
+  } catch (e) {
+    alert("Failed to fetch the input from storage");
+  }
+};
+
+useEffect(() => {
+  readData();
+}, []);
+
+const clearStorage = async () => {
+  try {
+    await AsyncStorage.clear();
+    alert("Storage successfully cleared!");
+  } catch (e) {
+    alert("Failed to clear the async storage.");
+  }
+};
+
+const onChangeText = value => setInput(value);
+
+const onSubmitEditing = () => {
+  if (!input) return;
+
+  saveData(input);
+  setInput("");
+};
+
+
 function CreateLoadoutScreen({ navigation }) {
+  const [input, setInput] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Create Loadout screen</Text>
+      <View style={styles.panel}>
+        <Text style={styles.label}>Enter your input here:</Text>
+        <TextInput
+          style={styles.inputField}
+          value={input}
+          placeholder="Enter"
+          onChangeText={onChangeText}
+          onSubmitEditing={onSubmitEditing}
+        />
+        <Text style={styles.text}>Your input is {input}</Text>
+        <Pressable onPress={clearStorage} style={styles.button}>
+          <Text style={styles.buttonText}>Clear Storage</Text>
+        </Pressable>
+      </View>
     </View>
   );
-}
+};
 
 const defaultStyles = {
   headerTintColor: "#fff", 
@@ -364,6 +426,34 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: "white",
     borderColor: "black"
+  },
+  panel: {
+    paddingTop: 10,
+    paddingHorizontal: 10,
+  },
+  label: {
+    fontSize: 20,
+  },
+  inputField: {
+    backgroundColor: "#fff",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#333",
+    width: "100%",
+    padding: 10,
+    marginTop: 12,
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "orange",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#444",
   },
 });
 
